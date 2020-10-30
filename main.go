5491ab167511c9d4f5ddae75a98b181a6cda736a
Patrick Marschlowitz <p.marschlowitz@gmail.com>
2020-10-29 21:54:07 -0700

    added create pokemon route

--- main.go
+++ main.go
@@ -13,7 +13,9 @@ func handleRequests() {
 	r := mux.NewRouter().StrictSlash(true)
 
 	r.HandleFunc("/", routes.HomePage)
-	r.HandleFunc("/articles", routes.AllArticles).Methods("GET")
+	r.HandleFunc("/pokemon", routes.GetAllPokemon).Methods("GET")
+	r.HandleFunc("/pokemon/type", routes.GetPokemonByType).Methods("GET")
+	r.HandleFunc("/pokemon", routes.CreatePokemon).Methods("POST")
 
 	fmt.Println("Server Running at 8081")
 	log.Fatal(http.ListenAndServe(":8081", r))
--- routes/routes.go
+++ routes/routes.go
@@ -3,18 +3,66 @@ package routes
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 
 	"github.com/Drasek-25/Rest_API/parseJson"
 )
 
+// ADD HTTP STATUS CODES
 var pokemon = parseJson.ParseJson()
 
-func AllArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: All Articles Endpoint")
-	json.NewEncoder(w).Encode(pokemon)
+func GetPokemonByType(w http.ResponseWriter, r *http.Request) {
+	log.Println("Endpoint Hit: GetPokemonByType")
+	keys, ok := r.URL.Query()["key"]
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'key' is missing")
+		json.NewEncoder(w).Encode("Missing Key")
+		return
+	}
+	fmt.Println("Searching for: ", keys[0])
+
+	var result []string
+
+	for i := 0; i < len(pokemon); i++ {
+		for j := 0; j < len(pokemon[i].Types); j++ {
+			if strings.ToLower(pokemon[i].Types[j]) == strings.ToLower(keys[0]) {
+				result = append(result, pokemon[i].Name)
+				break
+			}
+		}
+
+	}
+	if result == nil {
+		json.NewEncoder(w).Encode("Type Not Found")
+		fmt.Println("Search Failed")
+	} else {
+		json.NewEncoder(w).Encode(result)
+		fmt.Println("Search Succeded")
+	}
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Endpoint Hit")
+	fmt.Println("Endpoint Hit: HomePage")
+}
+
+func CreatePokemon(w http.ResponseWriter, r *http.Request) {
+	log.Println("Endpoint Hit: CreatePokemon")
+	var newPokemon parseJson.Pokemon
+	err := json.NewDecoder(r.Body).Decode(&newPokemon)
+	if err != nil {
+		fmt.Println("Invalid formatting")
+		json.NewEncoder(w).Encode("Invalid formatting")
+		return
+	}
+
+	pokemon = append(pokemon, newPokemon)
+	log.Println("CreatePokemon was Succesful")
+	json.NewEncoder(w).Encode(pokemon[len(pokemon)-1])
+}
+
+func GetAllPokemon(w http.ResponseWriter, r *http.Request) {
+	log.Println("Endpoint Hit: GetPokemonByType")
+	json.NewEncoder(w).Encode(pokemon)
 }
